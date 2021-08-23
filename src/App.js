99d6afdea812372b2c760aaa88c01f1e7bf90e75
IvanDodig <ivan.dodig.1998@gmail.com>
2021-08-23 17:41:53 +0200

    small fix

--- src/App.js
+++ src/App.js
@@ -1,45 +1,39 @@
-import React from 'react';
-import './App.css';
-import Navbar from './components/inc/Navbar';
-import Home from './components/pages/Home'; 
-import Signup from './components/pages/Signup'; 
-import Login from './components/pages/Login';
-import Footer from './components/inc/footer';
-import Car from './components/pages/Car';
-import {BrowserRouter as Router, Route, Switch } from 'react-router-dom';
-
-import { DatePickerComponent } from "@syncfusion/ej2-react-calendars";
-
+import React from "react";
+import "./App.css";
+import Navbar from "./components/inc/Navbar";
+import Home from "./components/pages/Home";
+import Signup from "./components/pages/Signup";
+import Login from "./components/pages/Login";
+import Footer from "./components/inc/footer";
+import Car from "./components/pages/Car";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 
 function App() {
-  return (
-    
-    <Router>
-    <div>
-        <Navbar />
-        <Switch>
+   return (
+      <Router>
+         <div>
+            <Navbar />
+            <Switch>
+               <Route exact path="/">
+                  <Home />
+               </Route>
 
-        <Route exact path = "/">
-          <Home />
-        </Route>
+               <Route path="/signup">
+                  <Signup />
+               </Route>
 
-        <Route path = "/signup">
-          <Signup />
-        </Route>
+               <Route path="/login">
+                  <Login />
+               </Route>
 
-        <Route path = "/login">
-          <Login />
-        </Route>
-
-        <Route path = "/car">
-          <Car />
-        </Route>
-
-        </Switch>
-        <Footer/>
-    </div>
-    </Router>
-  );
+               <Route path="/car">
+                  <Car />
+               </Route>
+            </Switch>
+            <Footer />
+         </div>
+      </Router>
+   );
 }
 
 export default App;
--- src/components/pages/Signup.js
+++ src/components/pages/Signup.js
@@ -1,58 +1,81 @@
-import React from 'react';
-import {TextField, Button, Divider} from '@material-ui/core';
-import {BrowserRouter as Router, Route, Switch, Link } from 'react-router-dom';
+import React from "react";
+import { BrowserRouter as Router, Route, Switch, Link } from "react-router-dom";
 
 function Signup() {
-    return(
-        <div>
-           <section className = "section">
-                <div className = "container col-md-4">
-                    <div className = "card shadow">
-                        <div className = "card-body">
-                            <div className = "row">
-                                <div className = "col-md-12">
-                                    <h6>Registriraj se</h6>
-                                    <hr />
-                                    <div className = "row">
-                                    <div className = "form-group col">
-                                        <label className = "mb-1">Ime</label>
-                                        <input type = "text" className = "form-control" placeholder = "Unesi Ime" />
-                                    </div>
-                                    <div className = "form-group col">
-                                        <label className = "mb-1">Prezime</label>
-                                        <input type = "text" className = "form-control" placeholder = "Unesi Prezime" />
-                                    </div>
-                                    </div>
-                                    <div className = "form-group">
-                                        <label className = "mb-1">Email</label>
-                                        <input type = "text" className = "form-control" placeholder = "Unesi Email" />
-                                    </div>
-                                    <div className = "form-group">
-                                        <label className = "mb-1">Telefonski Broj</label>
-                                        <input type = "text" className = "form-control" placeholder = "Unesi Telefonski broj" />
-                                    </div>
-                                    <div className = "form-group">
-                                        <label className = "mb-1">Lozinka</label>
-                                        <input type = "text" className = "form-control" placeholder = "Unesi Lozinku" />
-                                    </div>
-                                    <div className = "form-group py-2"> 
-                                    <Link to= "/login">Već imaš korisnički račun? Prijavi se.</Link>
-                                    </div>
-                                    <div className = "form-group py-3">
-                                        <button type = "button" className = "btn btn-primary shadow w-100">Registriraj se </button>
-                                    </div>
-                                    <div className = "col-md-6">
-
-                                    </div>
-                                </div>
-                            </div>
+   return (
+      <div>
+         <section className="section">
+            <div className="container col-md-4">
+               <div className="card shadow">
+                  <div className="card-body">
+                     <div className="row">
+                        <div className="col-md-12">
+                           <h6>Registriraj se</h6>
+                           <hr />
+                           <div className="row">
+                              <div className="form-group col">
+                                 <label className="mb-1">Ime</label>
+                                 <input
+                                    type="text"
+                                    className="form-control"
+                                    placeholder="Unesi Ime"
+                                 />
+                              </div>
+                              <div className="form-group col">
+                                 <label className="mb-1">Prezime</label>
+                                 <input
+                                    type="text"
+                                    className="form-control"
+                                    placeholder="Unesi Prezime"
+                                 />
+                              </div>
+                           </div>
+                           <div className="form-group">
+                              <label className="mb-1">Email</label>
+                              <input
+                                 type="text"
+                                 className="form-control"
+                                 placeholder="Unesi Email"
+                              />
+                           </div>
+                           <div className="form-group">
+                              <label className="mb-1">Telefonski Broj</label>
+                              <input
+                                 type="text"
+                                 className="form-control"
+                                 placeholder="Unesi Telefonski broj"
+                              />
+                           </div>
+                           <div className="form-group">
+                              <label className="mb-1">Lozinka</label>
+                              <input
+                                 type="text"
+                                 className="form-control"
+                                 placeholder="Unesi Lozinku"
+                              />
+                           </div>
+                           <div className="form-group py-2">
+                              <Link to="/login">
+                                 Već imaš korisnički račun? Prijavi se.
+                              </Link>
+                           </div>
+                           <div className="form-group py-3">
+                              <button
+                                 type="button"
+                                 className="btn btn-primary shadow w-100"
+                              >
+                                 Registriraj se{" "}
+                              </button>
+                           </div>
+                           <div className="col-md-6"></div>
                         </div>
-                    </div>
-                </div>
-
-            </section>
-        </div>
-    );
+                     </div>
+                  </div>
+               </div>
+            </div>
+         </section>
+      </div>
+   );
 }
 
 export default Signup;
